712a480d39bfa10a5dc949819e31f9f9b3c035cd
ronaldo silva` <silvarjronaldo@gmail.com>
2020-08-31 09:53:35 -0300

    refactor: versao lib

--- src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
+++ src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
@@ -3,7 +3,7 @@ package br.ce.wcaquino.taskbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TaskBackendApplication extends SpringBootServletInitializer {
